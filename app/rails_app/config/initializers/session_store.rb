5f247d3dd345e2386dd0e6098dd6bec708a42fc2
Jared Ning <ningj@bernie.local>
2009-11-07 12:43:00 -0600

    some cleanup.
    added application.css.

--- app/rails_app/config/initializers/session_store.rb
+++ app/rails_app/config/initializers/session_store.rb
@@ -5,7 +5,7 @@
 # Make sure the secret is at least 30 characters and all random, 
 # no regular words or you'll be exposed to dictionary attacks.
 ActionController::Base.session = {
-  :key         => '_rails_app_session',
+  :key         => '_catan_session',
   :secret      => '7f39efd9f449ea6e42eded68cced57f9da1ba61080e689cde7a540ff1409f25dd1fd074b20fdefcd2b17fe69c2566f954993ca514cae8c509b32d38491f61cb3'
 }
 
--- app/rails_app/test/unit/player_test.rb
+++ app/rails_app/test/unit/player_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class PlayerTest < ActiveSupport::TestCase
   
-  def test_starter_buildtings
+  def test_starter_buildings
     player = Player.make(:game => StandardGame.make)
     assert_equal 2, player.settlements.size
   end
