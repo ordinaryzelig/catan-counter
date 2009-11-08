23cebdf68f055088b03212115c2076bde8936b71
Jared Ning <ningj@bernie.local>
2009-11-08 11:26:56 -0600

    city can be downgraded to settlement.
    partial for action.

--- app/rails_app/app/controllers/cities_controller.rb
+++ app/rails_app/app/controllers/cities_controller.rb
@@ -1,2 +1,9 @@
 class CitiesController < ApplicationController
+  
+  def downgrade_to_settlement
+    @city = City.find(params[:id])
+    @city.downgrade_to_settlement
+    redirect_to game_url(@city.player.game)
+  end
+  
 end
--- app/rails_app/test/unit/city_test.rb
+++ app/rails_app/test/unit/city_test.rb
@@ -2,4 +2,19 @@ require 'test_helper'
 
 class CityTest < ActiveSupport::TestCase
   
+  def test_downgrade_to_settlement
+    player = Player.make
+    (player.settlements.left - 1).times { player.settlements.make }
+    city = player.cities.make
+    city.downgrade_to_settlement
+    assert_nil City.find_by_id(city)
+    assert_equal 5, player.settlements.size
+    # try with no settlements left to build.
+    assert !player.can_build_settlement?
+    city = player.cities.make
+    assert_difference('player.victory_points', -2) do
+      city.downgrade_to_settlement
+    end
+  end
+  
 end
