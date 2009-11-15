7406c293cc698d95469d8587334f5af51daf98bc
Jared Ning <ningj@bernie.local>
2009-11-15 14:31:11 -0600

    prettier, more functional knight images.

--- rails_app/config/routes.rb
+++ rails_app/config/routes.rb
@@ -16,6 +16,6 @@ ActionController::Routing::Routes.draw do |map|
   map.upgrade_to_city_settlement 'settlements/:id/upgrade_to_city', :controller => 'settlements', :action => 'upgrade_to_city', :conditions => {:method => :post}
   map.downgrade_to_settlement_city 'cities/:id/downgrade_to_settlement', :controller => 'cities', :action => 'downgrade_to_settlement', :conditions => {:method => :post}
   
-  map.resources :knights, :member => {:promote => :post}
+  map.resources :knights, :member => {:promote => :post, :toggle_activation => :put}
   
 end
