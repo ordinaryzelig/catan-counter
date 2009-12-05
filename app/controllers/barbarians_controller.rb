a913629bfb97d61efd91e81f42d245da95c88605
jared ning <public@ordinaryzelig.org>
2009-12-05 02:33:53 -0600

    better flash.
    better flash message for barbarian attack outcome.

--- app/controllers/barbarians_controller.rb
+++ app/controllers/barbarians_controller.rb
@@ -7,14 +7,20 @@ class BarbariansController < ApplicationController
     outcome = barbarians.attack
     flash[:notice] = []
     flash[:notice] << 'barbarians attacked'
-    flash[:notice] << "#{outcome}: #{outcome.strength_of_victorious_party} to #{outcome.strength_of_defeated_party}"
+    flash[:notice] << "#{outcome} (barbarians: #{outcome.strength_of_victorious_party}, knights: #{outcome.strength_of_defeated_party})"
     if outcome.barbarians_are_victorious?
       flash[:notice] << "#{outcome.players_with_weakest_army.map(&:name).to_sentence} lost a city"
     else
       if defender_of_catan = outcome.defender_of_catan
         flash[:notice] << "#{defender_of_catan.name} is the defender of catan"
       else
-        flash[:notice] << "#{outcome.players_with_strongest_army.map(&:name).to_sentence} best defended catan"
+        defenders = outcome.players_with_strongest_army(&:map)
+        defenders_string = if defenders.empty?
+          'nobody'
+        else
+          "#{defenders.size} players (#{defenders.map(&:name).to_sentence}) best"
+        end
+        flash[:notice] << "#{defenders_string} defended catan"
       end
     end
     redirect_to @game
